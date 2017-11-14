0e62757fb673ff7ac55110cfd594a58761658473
Angel162 <31974133+Angel162@users.noreply.github.com>
2017-11-13 20:26:01 -0400

    added example from opcv

---
+++ OPCV/app/src/main/java/com/example/angel/opcv/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.angel.opcv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    static {
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "Opencv not loaded");
+        } else {
+            Log.d(TAG, "Opencv loaded succesfully");
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
